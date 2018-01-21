b62de4114ac8b1a3f9dc2756df1b619c9c8e5bb0
Jean Barascu <narcis86@gmail.com>
2018-01-21 16:14:46 +0200

    Added into the project movies and genres data source as JSON file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Import movies list
+import Movies from './DataSource/movies.json';
+import Genres from './DataSource/genres.json';
+
 const TMDB_LOGO = 'https://www.themoviedb.org/static_cache/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png';
 
 const TMDB_WEBSITE= 'https://www.themoviedb.org/';
