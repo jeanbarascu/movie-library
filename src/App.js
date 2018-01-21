b4cae257a7d70bb53bf0c5ac2625e18b3aedb704
Jean Barascu <narcis86@gmail.com>
2018-01-21 16:45:50 +0200

    Created the footer.

--- src/App.js
+++ src/App.js
@@ -93,6 +93,12 @@ class App extends Component {
           </div>
         </div>
 
+        <footer>
+          <div className="col">
+            <span><i className="fa fa-github-square" aria-hidden="true"></i> <a href="https://github.com/jeanbarascu/movie-library" target="_blank" rel="noopener noreferrer">View Code</a></span>
+          </div>
+        </footer>
+
       </div>
     );
   }
